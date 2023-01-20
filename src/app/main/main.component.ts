381c51f9336dd66547ae238829a51c0d191e0b03
berregui ayoub <ayoubsetif01@gmail.com>
2023-01-20 18:37:44 +0100

    now working local storage

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -20,8 +20,6 @@ export class MainComponent implements OnInit {
 	) { }
 
 	ngOnInit() {
-		localStorage.setItem('prodconfig', "");
-		localStorage.setItem('config', '');
 	}
 
 	hideAndSeek(version) {
@@ -45,10 +43,10 @@ export class MainComponent implements OnInit {
 				const prodsheet = this.readFile(fileReader);
 				const prodarray = XLSX.utils.sheet_to_json(prodsheet, {raw: true });
 
-				//localStorage.setItem('prodconfig', JSON.stringify(_.keyBy(prodarray, 'id')));
+				localStorage.setItem('prodconfig', JSON.stringify(_.keyBy(prodarray, 'id')));
 
 				const config = _.keyBy(arr, 'clientDms');
-				//sslocalStorage.setItem('config', JSON.stringify(config));
+				localStorage.setItem('config', JSON.stringify(config));
 				this.snackBar.open('Configuration saved', 'Ok', { duration : 7000 });
 			};
 			fileReader.readAsArrayBuffer(this.file);
